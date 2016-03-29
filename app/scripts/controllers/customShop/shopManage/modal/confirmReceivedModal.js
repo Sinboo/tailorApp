cff34280edfa306f9e3d692ad44dc0c0e71f0692
王新彪 <sinboowang@163.com>
2016-03-29 22:48:01 +0800

    fix bug

--- app/scripts/controllers/customShop/shopManage/modal/confirmReceivedModal.js
+++ app/scripts/controllers/customShop/shopManage/modal/confirmReceivedModal.js
@@ -4,7 +4,8 @@
 'use strict';
 
 angular.module('tailorApp')
-  .controller('ConfirmReceivedModalCtrl', function ($scope, FABRIC_UNIT) {
+  .controller('ConfirmReceivedModalCtrl', function ($scope, FABRIC_UNIT, tailoringTypes) {
     $scope.FABRIC_UNIT = FABRIC_UNIT;
+    $scope.tailoringTypes = tailoringTypes;
 
   });
